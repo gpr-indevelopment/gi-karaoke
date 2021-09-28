91945ef4f6ef0b2fccafc331f1ee6438a3fb8b74
Gabriel Robaina <gabriel.pimenta@totvs.com.br>
2021-09-28 20:58:24 -0300

    Spring initializr

---
+++ backend/src/main/java/com/gprindevelopment/gikaraoke/GiKaraokeApplication.java
@@ -0,0 +1,13 @@
+package com.gprindevelopment.gikaraoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiKaraokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiKaraokeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gprindevelopment/gikaraoke/GiKaraokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gprindevelopment.gikaraoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiKaraokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
