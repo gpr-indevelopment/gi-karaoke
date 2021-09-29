78941c64ef6703294aa02a8c2765073be0f5c9dc
Gabriel Robaina <gabriel.pimenta@totvs.com.br>
2021-09-29 00:55:57 -0300

    Primeira build

--- backend/src/main/java/com/gprindevelopment/gikaraoke/KaraokeController.java
+++ backend/src/main/java/com/gprindevelopment/gikaraoke/KaraokeController.java
@@ -1,6 +1,7 @@
 package com.gprindevelopment.gikaraoke;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(KaraokeController.ROOT_URI)
+@CrossOrigin(origins = "*")
 public class KaraokeController {
 
     public static final String ROOT_URI = "/karaoke";
--- backend/src/main/java/com/gprindevelopment/gikaraoke/KaraokeQueueService.java
+++ backend/src/main/java/com/gprindevelopment/gikaraoke/KaraokeQueueService.java
@@ -2,6 +2,8 @@ package com.gprindevelopment.gikaraoke;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,6 +14,18 @@ import java.util.Queue;
 public class KaraokeQueueService {
 
     private Queue<SongRequest> songQueue = new LinkedList<>();
+
+    @PostConstruct
+    public void addValues() {
+//        SongRequest songRequest1 = new SongRequest("Song 1", "Request 1", LocalDateTime.now());
+//        SongRequest songRequest2 = new SongRequest("Song 2", "Request 2", LocalDateTime.now());
+//        SongRequest songRequest3 = new SongRequest("Song 3", "Request 3", LocalDateTime.now());
+//        SongRequest songRequest4 = new SongRequest("Song 4", "Request 4", LocalDateTime.now());
+//        enqueue(songRequest1);
+//        enqueue(songRequest2);
+//        enqueue(songRequest3);
+//        enqueue(songRequest4);
+    }
 
     public void enqueue(SongRequest songRequest) {
         songQueue.add(songRequest);
